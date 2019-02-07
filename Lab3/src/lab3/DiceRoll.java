56ab86da50a75a90a5fd30c7f636296e9900267a
1807132 <1807132@RG-N533-M03.rgu.ac.uk>
2019-02-07 10:45:32 +0000

    Final code

--- Lab3/src/lab3/DiceRoll.java
+++ Lab3/src/lab3/DiceRoll.java
@@ -8,10 +8,17 @@ package lab3;
 public class DiceRoll {
 
     public static void main(String[] args) {
-        int firstDice = (int)(Math.random() * 6) + 1;
-        int secondDice = (int)(Math.random() * 6) + 1;
-        System.out.println("Your first die comes up with " + firstDice);
-        System.out.println("Your second die comes up with " + secondDice);
-        System.out.println("Your total roll is " + (firstDice + secondDice));
-    }  
+        int greater = 0;
+        for(int i = 0; i < 10; i++) {
+            int firstDice = (int)(Math.random() * 6) + 1;
+            int secondDice = (int)(Math.random() * 6) + 1;
+            System.out.println("Your first die comes up with " + firstDice);
+            System.out.println("Your second die comes up with " + secondDice);
+            System.out.println("Your total roll is " + (firstDice + secondDice));
+            if ( firstDice > secondDice) {
+                greater++;
+            }
+            System.out.println("First dice had a score greater than the second dice " + greater + " times.");       
+        }   
+    }
 }
