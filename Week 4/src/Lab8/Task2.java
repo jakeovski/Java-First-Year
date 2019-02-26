f65db5d9b66e5a2b699e9783eee2531499e3936a
Pierre <Pierre@100.88.121.233>
2019-02-26 03:38:24 +0000

    Fully coded

--- Week 4/src/Lab8/Task2.java
+++ Week 4/src/Lab8/Task2.java
@@ -22,11 +22,21 @@ public class Task2 {
         for (int i = 0; i < arr.length; i++) {
             System.out.println("The sum of row " + (i +1) + " is " + rowSum(arr,i));
         }
+        for (int i = 0; i < arr[0].length; i++) {
+            System.out.println("The sum of column " + (i + 1) + " is " + colSum(arr,i));
+        }
     }
     public static int rowSum(int[][] arr , int row) {
         int sum = 0;
         for (int i = 0; i < arr[row].length;i++) {
             sum += arr[row][i];
+        }
+        return sum;
+    }
+    public static int colSum(int[][] arr, int col) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i][col];
         }
         return sum;
     }
