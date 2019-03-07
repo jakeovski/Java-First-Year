877a724438f55149ede319792ff9d2836eae62a4
Pierre <Pierre@100.88.121.233>
2019-03-07 20:07:11 +0000

    Fully coded

---
+++ Lab10/src/Task2/FootballTeam.java
@@ -0,0 +1,204 @@
+package Task2;
+
+import java.util.Arrays;
+
+
+/**
+ * A model of the Football Team
+ * @author Vladimir Bardadom
+ * @verison 1.0 7/03/2019
+ */
+
+public class FootballTeam {
+    
+    /**
+     * Surnames of the players
+     */
+    private String[] players;
+    
+    /**
+     * Color of the football kit
+     */
+    private String color;
+    
+    /**
+     * Goal difference
+     */
+    private int goal;
+    
+    /**
+     * Whether the team is knocked out from Trophy Challange
+     */
+    private boolean knockedOut;
+
+    /**
+     * The constructor for the football team
+     */
+    public FootballTeam() {
+        players = new String[5];
+        color = "red";
+        goal = 0;
+        knockedOut = false;
+    }
+
+    //-------------------Getter Methods-------------------
+    
+    /**
+     * Gets player surnames
+     * @return Array of player surnames
+     */
+    public String getPlayers() {
+        return Arrays.toString(players);
+    }
+
+    /**
+     * Gets color of the football kit
+     * @return Color of the football kit
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * Gets goal difference
+     * @return Goal difference
+     */
+    public int getGoal() {
+        return goal;
+    }
+
+    /**
+     * Gets the boolean if the team has been knocked out
+     * @return true if team is knocked out and false if not
+     */
+    public boolean isKnockedOut() {
+        return knockedOut;
+    }
+
+    //----------------------Setter Methods-----------------------
+    
+    /**
+     * Sets color of the football kit
+     * @param aColor Color of the football kit
+     */
+    public void setColor(String aColor) {
+        color = aColor;
+    }
+
+    /**
+     * Sets goal difference
+     * @param aGoal Goal difference
+     */
+    public void setGoal(int aGoal) {
+        goal = aGoal;
+    }
+
+    /**
+     * Sets if the team was knocked out or not
+     * @param aKnockedOut true if knocked out, false if not
+     */
+    public void setKnockedOut(boolean aKnockedOut) {
+        knockedOut = aKnockedOut;
+    }
+    
+    
+    //--------------------Other Methods----------------------------
+    
+    /**
+     * Adds player to the team
+     * @param player Player you want to add
+     * @return Array of the updated team
+     */
+    public String addPlayer(String player) {
+        int count = 0;
+        for (int i = 0; i < players.length; i++) {
+            if (players[i] == null) {
+                players[i] = player;
+                break;
+            }
+            else {
+               count++; 
+            }
+        }
+        if (count == 5) {
+            return "No more space to add players!";
+        }
+        else {
+            return Arrays.toString(players);
+        }
+    }
+    
+    /**
+     * Removes player from a team
+     * @param index Index of the player
+     * @return Message if the operatiomn was successful
+     */
+    public String removePlayer(int index) {
+        if (players[index] == null) {
+            return "There is nobody on that possition!";
+        }
+        else {
+            players[index] = "not assigned";
+            return "Removal was successful!";
+        }
+    }
+    
+    
+    /**
+     * Searches for a player in a team
+     * @param Item Player's surname
+     * @return true if found, false if not
+     */
+    public boolean searchItem(String Item) {
+        boolean found = false;
+        for (String player : players) {
+            if (player.equals(Item)) {
+                found = true;
+            }
+        }
+        return found;
+    }
+    
+    /**
+     * Replaces one player with another
+     * @param player Player you want to replace
+     * @param newPlayer Player you want to add instead
+     * @return Message of completion and new lineup of the team
+     */
+    public String replace(String player, String newPlayer) {
+        boolean found = false;
+        for (int i = 0; i < players.length; i++) {
+            if (players[i].equals(player)) {
+                players[i]= newPlayer;
+                found = true;
+            }
+        }
+        if (found = true) {
+            return "Replacement successful! New lineup: " + Arrays.toString(players);
+        }
+        else {
+            return "Player not found!";
+        }
+    }
+    
+    /**
+     * Checks if team is still in the competition
+     * @return Message if the team is still in the competition
+     */
+    public String competition() {
+        if (knockedOut == true) {
+            return "The team has been knocked out!";
+        }
+        else {
+            return "The team is still going!";
+        }
+    }
+    
+    /**
+     * toString method
+     * @return Information in the fields
+     */
+    public String toString() {
+        return "Team's lineup: " + getPlayers() + "\nColor of the football kit: " + getColor() + "\nCurrent goal difference: " + getGoal() + "\nStatus: " + competition();
+    }
+}
---
+++ Lab10/src/Task2/TestClass.java
@@ -0,0 +1,56 @@
+package Task2;
+
+
+/**
+ * Test Class for FootballTeam class
+ * @author Vladimir Bardadom
+ * @version 1.0 07/03/2019
+ */
+
+public class TestClass {
+    
+    public static void main(String[] args) {
+        
+        //Creating an instance of FootballTeam
+        FootballTeam team1 = new FootballTeam();
+        
+        //Test or getters
+        System.out.println("Lineup: " + team1.getPlayers());
+        System.out.println("Color of the kit: " + team1.getColor());
+        System.out.println("Goal difference: " + team1.getGoal());
+        System.out.println("Knocked Out? : " + team1.isKnockedOut());
+        
+        //Test the setters
+        team1.setColor("Bright red");
+        System.out.println("Color of the kit: " + team1.getColor());
+        team1.setGoal(3);
+        System.out.println("Goal difference: " + team1.getGoal());
+        team1.setKnockedOut(false);
+        System.out.println("Knocked Out? : " + team1.isKnockedOut());
+        
+        //Test add players method
+        System.out.println(team1.addPlayer("Kerzhakov"));
+        System.out.println(team1.addPlayer("Kokorin"));
+        System.out.println(team1.addPlayer("Mamaev"));
+        System.out.println(team1.addPlayer("Golovin"));
+        System.out.println(team1.addPlayer("Dzyuba"));
+        System.out.println(team1.addPlayer("Maddyson"));
+        
+        //Test remove player method
+        System.out.println("Remove Mamaev from the team: " + team1.removePlayer(2));
+        
+        //Test searc item method 
+        System.out.println("Checking if holy Dzyuba is in the team: " + team1.searchItem("Dzyuba"));
+        
+        //Test replace method
+        System.out.println("Checking if replacement of Kokorin to Maddyson was successful: " + team1.replace("Kokorin", "Maddyson"));
+        
+        //Test competition method
+        System.out.println("Checking if team is still in competition: " + team1.competition());
+        
+        //Test toString method
+        System.out.println(team1.toString());
+        
+        
+    }
+}
